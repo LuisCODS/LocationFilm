952f350edb8572e8dde099811401a933d8e868d6
Luis <tennis.bresil@gmail.com>
2020-11-15 22:18:10 -0500

    DEPOT LABO 2 EN SPA

---
+++ movieSPA/movies/BD/backup_bdfilms.sql
@@ -0,0 +1,152 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Banco de dados: `bdfilms`
+--
+CREATE DATABASE IF NOT EXISTS `bdfilms` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `bdfilms`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `categories`
+--
+
+INSERT INTO `categories` (`id`, `titre`, `slug`, `created_at`, `updated_at`) VALUES
+(1, 'Action', 'action', '2020-11-08 06:31:38', '2020-11-08 06:31:49'),
+(2, 'Romance', 'romance', '2020-11-08 08:12:37', '2020-11-08 08:12:37'),
+(9, 'Horreur', 'horreur', '2020-11-09 08:31:18', '2020-11-09 08:31:18'),
+(10, 'Drame', 'drame', '2020-11-09 08:31:42', '2020-11-09 08:31:42');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `films`
+--
+
+DROP TABLE IF EXISTS `films`;
+CREATE TABLE IF NOT EXISTS `films` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
+  `titre` varchar(255) NOT NULL DEFAULT '',
+  `image` text NOT NULL,
+  `preview_url` text NOT NULL,
+  `auteur` varchar(255) DEFAULT NULL,
+  `duree` varchar(10) DEFAULT NULL,
+  `prix` int(11) UNSIGNED NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `films`
+--
+
+INSERT INTO `films` (`id`, `category_id`, `titre`, `image`, `preview_url`, `auteur`, `duree`, `prix`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Superman', '92ea353a22d8540c4b333b7fa890a9e71bf192fb.jpg', 'https://www.youtube.com/watch?v=T6DJcgm3wNY', 'Jerry Sieg', '3h 8m', 10, '2020-11-08 06:30:53', '2020-11-08 06:30:53'),
+(2, 1, 'Dark Tower', '022cc761e7338535863dd34464cb085bd2c029ec.jpeg', 'https://www.youtube.com/watch?v=GjwfqXTebIY', 'Nikolaj Arcel', '1h 35m', 20, '2020-11-08 06:35:36', '2020-11-08 06:35:36'),
+(3, 1, 'Amazing Spiderma 3', '6b40a95e6886bc538962ff5abad36e4c6e225409.jpeg', 'https://www.youtube.com/watch?v=Y51d7UsxfKY', 'Jon Watts', '2h 13m', 15, '2020-11-08 06:36:08', '2020-11-08 06:36:08'),
+(4, 2, 'Aladdin (2019)', 'ac128a1c3a3f4032bb835366a6c6a9077ccba20c.jpg', 'https://www.youtube.com/watch?v=foyufD52aog', 'Guy Ritchie', '2h 8m', 30, '2020-11-08 06:39:08', '2020-11-08 06:39:08'),
+(5, 2, 'Le Diable S\'habille en Prada', '9827fbe7af3835f1363a2394f6c643ce27d8868b.jpg', 'https://www.youtube.com/watch?v=SkXERnN-vzw', 'David Frankel', '1h 50m', 25, '2020-11-08 06:40:40', '2020-11-08 06:40:40'),
+(6, 2, 'Focus', 'b39a4f6316265e04303e07138cc8d6544d5831f8.jpg', 'https://www.youtube.com/watch?v=MxCRgtdAuBo', 'Denise Di Novi', '1h 45m', 8, '2020-11-08 06:40:40', '2020-11-08 06:40:40'),
+(30, 10, 'Lion', '710701e439370e0a335478c53696652c4e0a7e0d.jpg', 'https://www.youtube.com/watch?v=mD23AJkyq6g', 'Garth Davis', '2h', 10, '2020-11-09 08:34:34', '2020-11-09 08:34:34');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+CREATE TABLE IF NOT EXISTS `locations` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `film_id` int(11) NOT NULL,
+  `membre_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `locations`
+--
+
+INSERT INTO `locations` (`id`, `film_id`, `membre_id`, `quantity`, `created_at`, `updated_at`) VALUES
+(94, 2, 1, NULL, '2020-11-10 07:00:11', '2020-11-10 07:00:11');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `membres`
+--
+
+DROP TABLE IF EXISTS `membres`;
+CREATE TABLE IF NOT EXISTS `membres` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `is_admin` int(11) DEFAULT 0,
+  `password` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `membres`
+--
+
+INSERT INTO `membres` (`id`, `nom`, `email`, `is_admin`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Antonio', 'atavares@cmaisonneuve.qc.ca', 0, '$2y$10$MxuvrN8Dc0ZQ6A5hIbfiPeKYtc4C3k3Xx1ER22NSNTZ43ZfzP2/Cq', '2020-11-09 03:39:58', '2020-11-08 07:39:03'),
+(2, 'luis', 'tennis.bresil@gmail.com', 0, '$2y$10$MxuvrN8Dc0ZQ6A5hIbfiPeKYtc4C3k3Xx1ER22NSNTZ43ZfzP2/Cq', '2020-11-09 03:37:02', '2020-11-08 07:39:03'),
+(3, 'admin', 'admin@gmail.com', 1, '$2y$10$h1Pg/PTwAdytvUEqIomcKOqxRlaIEBuokcTG2..vYOxrEPu0Ws31S', '2020-11-09 04:40:05', '2020-11-08 12:34:22'),
+(4, 'membre', 'membre@gmail.com', 0, '$2y$10$xnB8OxnsRUNWnFPWRUsIv.Ocq0bcKW3FyRSjvVVNeX85cd46BkWHO', '2020-11-10 01:13:22', '2020-11-10 01:13:22');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `rentals`
+--
+
+DROP TABLE IF EXISTS `rentals`;
+CREATE TABLE IF NOT EXISTS `rentals` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `film_id` int(11) NOT NULL,
+  `membre_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `rentals`
+--
+
+INSERT INTO `rentals` (`id`, `film_id`, `membre_id`, `quantity`, `created_at`, `updated_at`) VALUES
+(24, 1, 4, 1, '2020-11-16 02:50:03', '2020-11-16 02:50:03'),
+(25, 3, 4, 1, '2020-11-16 02:50:03', '2020-11-16 02:50:03'),
+(26, 2, 2, 1, '2020-11-16 02:51:03', '2020-11-16 02:51:03');
+COMMIT;
---
+++ movieSPA/movies/viewsfilms/includes/menu.php
@@ -0,0 +1,64 @@
+<?php
+  include_once("includes/db.php");
+  $stmt = $conn->prepare('SELECT * FROM categories');
+  $stmt->execute();
+  $categories = $stmt->fetchAll();
+  $errorMsg = "";
+  try {
+    if(isset($_POST["search"])){
+      $films = search($_POST["query"], $conn);
+    }
+  }
+  catch(PDOException $error){
+    $errorMsg = $error->getMessage();
+  }
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="list.php">Films</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="home.php">
+          <i class="fa fa-home" aria-hidden="true"></i>
+          Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="list.php">
+          <i class="fa fa-film" aria-hidden="true"></i>
+          Nos Films</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">
+          <i class="fa fa-list" aria-hidden="true"></i>
+          Cateogires
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <?php foreach($categories as $category){ ?>
+          <a class="dropdown-item" href="#"
+            onclick="getFilmsByCat('<?php echo $category['slug']; ?>','<?php echo $category['titre']; ?>')"><?php echo $category["titre"]; ?></a>
+          <?php } ?>
+        </div>
+      </li>
+      <form method="post" class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" name="query" placeholder="Rechercher..." aria-label="Search"
+          required>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="search">Rechercher</button>
+      </form>
+    </ul>
+    <?php if(checkIfLogedIn()){ ?>
+    <a class="navbar-brand" href="admin/panier.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Mon
+      Panier</a>
+    <a class="navbar-brand" href="#"><i class="fa fa-user" aria-hidden="true"></i> <?php echo $_SESSION["email"]; ?></a>
+    <a class="navbar-brand" href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Deconnexion</a>
+    <?php } else{ ?>
+    <a class="navbar-brand" href="register.php"><i class="fa fa-user-plus" aria-hidden="true"></i> Devenir Membre</a>
+    <a class="navbar-brand" href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Connexion</a>
+    <?php }?>
+  </div>
+</nav>
